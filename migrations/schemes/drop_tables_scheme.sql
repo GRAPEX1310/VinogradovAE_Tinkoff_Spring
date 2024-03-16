30bfba2c4c06f898536b8e145ad36f274e0efa58
VinogradovAE <sashavin31@yandex.ru>
2024-03-16 18:54:03 +0300

    Правки ДЗ№4

--- migrations/schemes/drop_tables_scheme.sql
+++ migrations/schemes/drop_tables_scheme.sql
@@ -4,13 +4,7 @@
 DROP TABLE IF EXISTS users CASCADE;
 
 --changeset VinogradovAE:id2
-DROP TABLE IF EXISTS all_links CASCADE;
+DROP TABLE IF EXISTS links CASCADE;
 
 --changeset VinogradovAE:id3
-DROP TABLE IF EXISTS github_links CASCADE;
-
---changeset VinogradovAE:id4
-DROP TABLE IF EXISTS stackoverflow_links CASCADE;
-
---changeset VinogradovAE:id5
 DROP TABLE IF EXISTS user_links CASCADE;
--- scrapper/src/test/java/edu/java/scrapper/SimpleIntegrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/SimpleIntegrationTest.java
@@ -11,39 +11,28 @@ import java.sql.Statement;
 import static org.hibernate.validator.internal.util.Contracts.assertTrue;
 
 @Testcontainers
-public class SimpleIntegrationTest extends IntegrationEnvironment{
+public class SimpleIntegrationTest extends IntegrationEnvironment {
 
     @Test
     @DisplayName("Test Correct Tables Creation Inside Container")
     public void testCorrectTablesCreationInsideContainer() {
         try (
-            Connection connection = DriverManager.getConnection(POSTGRES.getJdbcUrl(), POSTGRES.getUsername(), POSTGRES.getPassword());
+            Connection connection = DriverManager.getConnection(
+                POSTGRES.getJdbcUrl(),
+                POSTGRES.getUsername(),
+                POSTGRES.getPassword()
+            );
             Statement statement = connection.createStatement()
         ) {
-            ResultSet resultSet = statement.executeQuery("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'users')");
+            ResultSet resultSet = statement.executeQuery(
+                "SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'users')");
             resultSet.next();
             assertTrue(resultSet.getBoolean(1), "Table 'users' does not exist");
 
-            ResultSet resultSetAllLinks = statement.executeQuery("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'all_links')");
+            ResultSet resultSetAllLinks = statement.executeQuery(
+                "SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'links')");
             resultSetAllLinks.next();
-            assertTrue(resultSetAllLinks.getBoolean(1), "Table 'all_links' does not exist");
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Test
-    @DisplayName("Test Existing Data In Tables Inside Container")
-    public void testExistingDataInTablesInsideContainer() {
-        try (
-            Connection connection = DriverManager.getConnection(POSTGRES.getJdbcUrl(), POSTGRES.getUsername(), POSTGRES.getPassword());
-            Statement statement = connection.createStatement()) {
-
-            ResultSet resultSetUsersQuantity = statement.executeQuery("SELECT COUNT(*) AS quantity FROM users");
-            resultSetUsersQuantity.next();
-            assertTrue(resultSetUsersQuantity.getInt("quantity") == 3, "Error occured");
-
+            assertTrue(resultSetAllLinks.getBoolean(1), "Table 'links' does not exist");
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
