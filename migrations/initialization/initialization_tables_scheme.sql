7d876dc79b0b5fef51f88ff583832d14b5da10f3
VinogradovAE <sashavin31@yandex.ru>
2024-03-27 20:53:15 +0300

    Решение конфликтов

--- migrations/initialization/initialization_tables_scheme.sql
+++ migrations/initialization/initialization_tables_scheme.sql
@@ -11,14 +11,13 @@ CREATE TYPE enum_link_type AS ENUM
 --changeset VinogradovAE:create_table_users
 CREATE TABLE IF NOT EXISTS users
 (
-    user_id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    chat_id BIGINT UNIQUE NOT NULL
+    id BIGINT UNIQUE NOT NULL
 );
 
 --changeset VinogradovAE:create_table_links
 CREATE TABLE IF NOT EXISTS links
 (
-    link_id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     link_type enum_link_type NOT NULL,
     url VARCHAR(128) UNIQUE NOT NULL
 );
@@ -26,7 +25,7 @@ CREATE TABLE IF NOT EXISTS links
 --changeset VinogradovAE:create_table_user_links
 CREATE TABLE IF NOT EXISTS user_links
 (
-    user_id BIGINT REFERENCES users(user_id),
-    link_id BIGINT REFERENCES links(link_id),
+    user_id BIGINT REFERENCES users(id),
+    link_id BIGINT REFERENCES links(id),
     PRIMARY KEY (user_id, link_id)
 );
